a3e3a737fb3e46bc39585aee540ada457aac6994
viraj-parmar-python-ak <viraj.parmar@armakuni.com>
2026-01-29 15:32:26 +0530

    feat: Implemented String with memory allocation differences

---
+++ string.py
@@ -0,0 +1,29 @@
+# Memory Maping in the String
+
+# t = 'python'
+# print(id(t))
+# print("0: ",id(t[0]))
+# print("1: ",id(t[1]))
+# t = 'P' + t[1:]
+# print("After changing the value in the string")
+# print(id(t))
+# print("0: ",id(t[0]))
+# print("1: ",id(t[1]))
+
+
+# a = "this "
+# b = "string"
+
+# print("id of a:", id(a))
+# print("id of b:", id(b))
+
+# print("id of a[0]:", id(a[0]))
+# print("id of b[0]:", id(b[0]))
+
+# c = a + b
+# print(c)
+
+# print("id of c:", id(c))
+
+# print("id of a[0] in c:", id(c[0]))
+# print("id of b[0] in c:", id(c[5]))
