8dca87515692c9f730afbf3da2277ee2046052cd
viraj-parmar-python-ak <viraj.parmar@armakuni.com>
2026-01-30 15:05:47 +0530

    update: Implemented memory allocation of string

--- my_string.py
+++ my_string.py
@@ -11,19 +11,19 @@
 # print("1: ",id(t[1]))
 
 
-# a = "this "
-# b = "string"
+a = "this "
+b = "string"
 
-# print("id of a:", id(a))
-# print("id of b:", id(b))
+print("id of a:", id(a))
+print("id of b:", id(b))
 
-# print("id of a[0]:", id(a[0]))
-# print("id of b[0]:", id(b[0]))
+print("id of a[0]:", id(a[0]))
+print("id of b[0]:", id(b[0]))
 
-# c = a + b
-# print(c)
+c = a + b
+print(c)
 
-# print("id of c:", id(c))
+print("id of c:", id(c))
 
-# print("id of a[0] in c:", id(c[0]))
-# print("id of b[0] in c:", id(c[5]))
+print("id of a[0] in c:", id(c[0]))
+print("id of b[0] in c:", id(c[5]))
