d38f191a33489a49bea461fa719dcf338eee25b8
Viraj Parmar <viraj.parmar@armakuni.com>
2026-02-02 15:28:53 +0530

    fix: added comments and doc strings to functions
    Added docstrings for create_new_dir and create_file functions. Improved comments for clarity.

--- os_module.py
+++ os_module.py
@@ -1,22 +1,30 @@
 import os
 
 def create_new_dir(new_dir):
+    '''
+    Create a new directory
+    @return: true if directory id successfully created
+    @return: false in case of error
+    '''
     current_dir = os.getcwd()
 
     try:
         path = os.path.join(current_dir, new_dir)
         os.mkdir(path)
-        os.chdir(path)
-        status = True
+        os.chdir(path)       # change the current path to new dir
+        status = True        # dir created successfully
         print(f"Dir created at {path}")
     except:
         print(f"Seems like directory named '{new_dir}' already exist")
-        status = False
+        status = False        # dir with the same name already exist
     finally:
         return status
     
 
 def create_file(file_name):
+    '''
+    Create new empty file
+    '''
     try:
         file = open(file_name, "w")
         print("File created!")
@@ -27,9 +35,9 @@ def create_file(file_name):
 
 
 
-dir = input("Enter the name of directory you want to create: ")
-creation_status = create_new_dir(dir)
+dir = input("Enter the name of directory you want to create: ")            # taking directory name from the user
+creation_status = create_new_dir(dir)                                      # if status is true then dir is created
 
-while(creation_status == False):
-    dir = input("Enter some other name of the directory: ")
+while(creation_status == False):                                           # if status is false
+    dir = input("Enter some other name of the directory: ")                # ask user to enter new unique name of directory
     creation_status = create_new_dir(dir)
